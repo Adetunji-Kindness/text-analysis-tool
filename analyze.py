e01d8b9f2387d53eab46ecd10c2126d68821e13d
Adetunji Kindness <adetunjikindness@gmail.com>
2026-01-19 22:34:42 +0100

    Set up 3 attempt username error

--- analyze.py
+++ analyze.py
@@ -6,15 +6,30 @@ def welcomeUser():
 
 # Get Username
 def getUsername():
-    # Print message prompting user to input their name
-    usernameFromInput = input("\nTo begin, please enter your username")
 
+    maxAttempts = 3
+    attempt = 0
+
+    while attempts > maxAttempts:
+
+    # Print message prompting user to input their name
+    inputPrompt = ""
+    if attempts ==0
+        inputPrompt = "\nTo begin, please enter your username:\n"
+    else:
+        inputPrompt = "\nPlease try again:\n"
+        usernameFromInput = input(inputPrompt)
+
+    # Validate username
     if len(usernameFromInput) > 5 or not usernameFromInput.isidentifier():
         print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces and cannot start with a number!")
-        print("Assigning new username...")
-        return generate_username()[0]
+    else:
+        return usernameFromInput
 
-    return usernameFromInput
+    attempts += 1
+   
+    print("\nExhausted all " + str(maxAttempts) + " attempts, assigning username instead...")
+        return generate_username()[0]
 
 # Greet the user
 def greetUser(name):
@@ -22,4 +37,4 @@ def greetUser(name):
 
 welcomeUser()
 username = getUsername()
-greetUser(username)
+greetUser(username) 
