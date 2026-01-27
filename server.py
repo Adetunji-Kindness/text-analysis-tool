a00af40835d2a943f930cb38bbf38f7f04e972bb
Adetunji Kindness <adetunjikindness@gmail.com>
2026-01-27 16:21:24 +0100

    Added test stock analysis json payload

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 # Importing flask module in the project is mandatory
 # An object of flask class is our WSGI application
 from flask import Flask, abort, request
+from flask_cors import CORS
 from stockAnalyze import getCompanyStockInfo
 from analyze import analyzeText
+import json
+
+f = open('test/result.json')
+stockDataTest = json.load(f)
 
 # Flask constructor takes the name of
 # current module (_name_) as arument.
 app = Flask(_name_)
+CORS(app)
 
 # The route() function of the flask class is a decorator,
 # which tells the application which URL should call
@@ -17,6 +23,7 @@ def healthCheck():
 
 @app.route('/analyze-stock/<ticker>', methods=['GET'])
 def analyzeStock(ticker):
+    return stockDataTest
     if len(ticker) > 5 or not ticker.isidentifier():
         abort(400, 'Invalid ticker symbol')
     try:
